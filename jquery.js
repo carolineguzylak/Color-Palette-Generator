f88cb801cc81ae77c34dc89937cfad7ad9a2a6cc
carlyguzylak <74155787+carolineguzylak@users.noreply.github.com>
2021-08-17 11:54:12 -0700

    Single color and Large Palette Generator and functioning properly.

--- jquery.js
+++ jquery.js
@@ -8,6 +8,29 @@ function randomColor(){
     return color;
 };
 
+// $("#singleColorBox").draggable();
+
+// $("#palette").droppable({
+//     drop: function(event, ui){
+//       $(this).css("background-color","#21dd85");
+//       $(this).find("p").remove();
+//       $("<li></li>").text(ui.draggable.text()).appendTo(this);
+//     },
+//     accept: ".color"
+//   }
+
 $("#singleColorButton").click(function(){
     $("#singleColorBox").css("background-color", randomColor());
 });
+
+$("#largePaletteButton").click(function(){
+    $("#LPcolor1").css("background-color", randomColor());
+    $("#LPcolor2").css("background-color", randomColor());
+    $("#LPcolor3").css("background-color", randomColor());
+    $("#LPcolor4").css("background-color", randomColor());
+    $("#LPcolor5").css("background-color", randomColor());
+    $("#LPcolor6").css("background-color", randomColor());
+    $("#LPcolor7").css("background-color", randomColor());
+    $("#LPcolor8").css("background-color", randomColor());
+});
+
